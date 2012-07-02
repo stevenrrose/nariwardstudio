b674605fbcb6e207aa8eff6bda189610a5aa2330
Robert Rose <rrose1@indiana.edu>
2012-07-02 12:50:04 -0400

    Simplification of the splash page.

--- js/index.js
+++ js/index.js
@@ -1,20 +1,7 @@
-// Requires MooTools 1.3
-function indexEvents() {
-
-  var splash2 = $("splash2");
-  var trigger = $("trigger");
-
-  // The IE "filter" may or may not be working.
-  var splash2In = function(){
-    var effect = new Fx.Morph(splash2, {duration: 1500});
-    effect.start(
-      {"opacity": 1, 
-       "filter": "alpha(opacity=1)",
-       "z-index": 2}
-    );
-  };
+$(document).ready(
   
-  trigger.addEvent("mouseover", splash2In);
-};
+  function(){ 
+    $("#splashimage").fadeIn(2000);
+  }
 
-window.addEvent('domready', indexEvents);
+);
